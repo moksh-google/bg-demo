1736f3d1beac3923e1ec4429cc95197de403a855
Moksh Atukuri <mokshazna@google.com>
2023-06-21 13:06:02 +0000

    new variable

--- includes/variables.js
+++ includes/variables.js
@@ -1,7 +1,9 @@
 const fruit_desc=`This is the fruit name`;
 const ingestion_time_desc=`This is the ingestion time for the record`;
+const another_var=`Testing the new const`;
 
 module.exports ={
     fruit_desc,
-    ingestion_time_desc
+    ingestion_time_desc,
+    another_var
 };
