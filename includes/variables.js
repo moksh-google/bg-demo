138b56ba81c755c2075e45622fab492b5d9bd050
Moksh Atukuri <137258142+orpzs@users.noreply.github.com>
2023-11-15 15:14:22 +0530

    Moksh trying the stuff

--- includes/variables.js
+++ includes/variables.js
@@ -1,6 +1,7 @@
 const fruit_desc=`This is the fruit name`;
 const ingestion_time_desc=`This is the ingestion time for the record`;
 const another_var=`Testing the new const`;
+const test_var="Testing this from cloudtop"
 
 module.exports ={
     fruit_desc,
